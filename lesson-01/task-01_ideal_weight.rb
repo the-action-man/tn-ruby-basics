8e56c52a7c66a68f0934f72b1761dc63cf987b37
Andrei <the.action.man@yandex.com>
2019-08-09 02:59:40 +0300

    lesson-01 task-1: Ideal weight

---
+++ lesson-01/task-01_ideal_weight.rb
@@ -0,0 +1,12 @@
+puts 'Enter your name'
+name = gets.chomp
+
+puts 'Enter your height'
+height = gets.to_i
+
+ideal_weight = height - 110
+if ideal_weight < 0 
+	puts "Your weight is already optimal"
+else
+	puts "#{name}, your ideal weight is #{ideal_weight}"
+end
