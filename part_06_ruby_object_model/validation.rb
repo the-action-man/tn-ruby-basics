3c56ce94af2f8d451b7aa67db81edf643761a055
Andrei <the.action.man@yandex.com>
2019-10-03 23:55:00 +0300

    Part 10: improvements

--- part_06_ruby_object_model/validation.rb
+++ part_06_ruby_object_model/validation.rb
@@ -27,41 +27,29 @@ module Validation
 
     def validate!
       variables_to_validate = self.class.variables_to_validate
-      variables_to_validate.each do |args|
-        variable_name = args[0]
-        validation_type = args[1]
-        validation_arg = args[2]
-        validate_variable(variable_name, validation_type, validation_arg)
-      end
+      variables_to_validate.each(&method(:validate_variable))
     end
 
-    def validate_variable(variable_name, validation_type, validation_arg)
+    def validate_variable(args)
+      variable_name, validation_type, validation_arg = args
       value = instance_variable_get("@#{variable_name}")
-
-      case validation_type
-      when :presence
-        validate_presence(variable_name, value)
-      when :format
-        validate_format(variable_name, value, validation_arg)
-      when :type
-        validate_type(variable_name, value, validation_arg)
-      else
-        raise RailRoadError, "'#{:type}' is unknown validation type" \
-                             "(second argument) for variable #{variable_name}"
-      end
+      self.send("validate_#{validation_type}", variable_name, value, validation_arg)
     end
 
-    def validate_presence(variable_name, value)
+    def validate_presence(*args)
+      variable_name, value = args
       raise RailRoadError, "#{variable_name} value is nil" if value.nil?
       raise RailRoadError, "#{variable_name} value is empty" if value.empty?
     end
 
-    def validate_format(variable_name, value, format)
+    def validate_format(*args)
+      variable_name, value, format = args
       raise RailRoadError, "#{variable_name} value has wrong format. " \
                            "Expected format: #{format}" if value !~ format
     end
 
-    def validate_type(variable_name, value, expected_class)
+    def validate_type(*args)
+      variable_name, value, expected_class = args
       raise RailRoadError, "#{variable_name} variable has wrong class. " \
                            "Expected class: #{expected_class}" \
                            unless value.instance_of? expected_class
